656c3aa6b7de68895ed40fcd9d7a3b5045125a00
Kyle <Kyle.Flavin@gmail.com>
2016-07-05 19:37:49 -0700

    Initial commit.

---
+++ django_social/django_social/urls.py
@@ -0,0 +1,26 @@
+"""django_social URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+from django_social_app.views import login, home, logout
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url('', include('social.apps.django_app.urls', namespace='social')),
+    url(r'^$', login),
+    url(r'^home/$', home),
+    url(r'^logout/$', logout),
+]
